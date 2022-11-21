0ee7eafa6ed92c71827f7d9e67524b57715ed0c9
ys4136 <ys4136@nyu.edu>
2022-11-21 01:27:33 -0500

    medium message

--- week11/script.js
+++ week11/script.js
@@ -1,138 +1,46 @@
-var party = new Array() 
-party[0] = new Image()
-party[0].src = "https://cdn.glitch.global/ffd19a77-d2f9-4922-8660-ef6e5360b0b0/DANCEPARTY-01.png?v=1668297218576" // set image src property to image path, preloading image in the process
-party[1] = new Image()
-party[1].src = "https://cdn.glitch.global/ffd19a77-d2f9-4922-8660-ef6e5360b0b0/dance1-01.png?v=1668297213793"
-party[2] = new Image()
-party[2].src = "https://cdn.glitch.global/ffd19a77-d2f9-4922-8660-ef6e5360b0b0/dance2-01.png?v=1668297215845"
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
 
-var discoball = new Image();
-discoball.src = 'https://cdn.glitch.global/ffd19a77-d2f9-4922-8660-ef6e5360b0b0/disco_ball_rotating_300_wht.gif?v=1668011564345';
+let draggedItem = null;
 
+for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
 
-var step=0
+	item.addEventListener('dragstart', function () {
+		draggedItem = item;
+		setTimeout(function () {
+			item.style.display = 'none';
+		}, 0)
+	});
 
-function slideit(){
-if (!document.images)
-return
-document.getElementById('slide').src = party[step].src
-if (step<2)
-step++
-else
-step=0
+	item.addEventListener('dragend', function () {
+		setTimeout(function () {
+			draggedItem.style.display = 'block';
+			draggedItem = null;
+		}, 0);
+	})
 
-setTimeout("slideit()",100)
-}
+	for (let j = 0; j < lists.length; j ++) {
+		const list = lists[j];
 
+		list.addEventListener('dragover', function (e) {
+			e.preventDefault();
+		});
+		
+		list.addEventListener('dragenter', function (e) {
+			e.preventDefault();
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+		});
 
-var count = 0;
-//cricket
-var cricket = document.getElementById('cricket')
-cricket.play();
-//drum
-var drum = document.getElementById('drum');
-var playPauseBTN1 = document.getElementById('playPauseBTN1');
-function playPauseDrum(){
-  if (count==0){
-    count = 1;
-    drum.play();  
-    step=0
-    slideit();
-    cricket.pause();
+		list.addEventListener('dragleave', function (e) {
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+
+		list.addEventListener('drop', function (e) {
+			console.log('drop');
+			this.append(draggedItem);
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+		});
+	}
   
-  }else{
-    count = 0;
-    drum.pause();  
-    step=5;
-    cricket.play();
-  }
 }
-
-//hiphop 
-var hiphop = document.getElementById('hiphop');
-var playPauseBTN2 = document.getElementById('playPauseBTN2');
-function playPauseHiphop(){
-  if (count==0){
-    count = 1;
-    hiphop.play();
-    step=0
-    slideit();
-    cricket.pause();
-  }else{
-    count = 0;
-    hiphop.pause();
-    step=5;
-    cricket.play();
-  }
-}
-//melodicBeat
-var melodicBeat = document.getElementById('melodicBeat');
-var playPauseBTN3 = document.getElementById('playPauseBTN3');
-function playPauseMelodicBeat(){
-  if (count==0){
-    count = 1;
-    melodicBeat.play();
-    step=0
-    slideit();
-    cricket.pause();
-  }else{
-    count = 0;
-    melodicBeat.pause();
-    step=5;
-    cricket.play();
-  }
-}
-//funk
-var funk = document.getElementById('funk');
-var playPauseBTN4 = document.getElementById('playPauseBTN4');
-function playPauseFunk(){
-  if (count==0){
-    count = 1;
-    funk.play();
-    step=0
-    slideit();
-    cricket.pause();
-  }else{
-    count = 0;
-    funk.pause();
-    step=5;
-    cricket.play();
-  }
-}
-//electro Pop
-var electroPop = document.getElementById('electroPop');
-var playPauseBTN5 = document.getElementById('playPauseBTN5');
-function playPauseElectroPop(){
-  if (count==0){
-    count = 1;
-    electroPop.play();
-    step=0
-    slideit();
-    cricket.pause();
-  }else{
-    count = 0;
-    electroPop.pause();
-    step=5;
-    cricket.play();
-  }
-}
-//east coast drum
-var eastcoastDrum = document.getElementById('eastcoastDrum');
-var playPauseBTN6 = document.getElementById('playPauseBTN6');
-function playPauseEastcoastDrum(){
-  if (count==0){
-    count = 1;
-    eastcoastDrum.play();
-    step=0
-    slideit();
-    cricket.pause();
-  }else{
-    count = 0;
-    eastcoastDrum.pause();
-    step=5;
-    cricket.play();
-  }
-}
-
-
-
